84b722ed1b550b1adc76fdd26c7a823d371ae60a
AlexGz9851 <a00820225@itesm.mx>
2021-02-20 21:23:44 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -1,20 +1,23 @@
 
+def checkNeg(args):
+  errMsg = "Negatives not allowed:"
+  for i in args:
+    if i< 0:
+      errMsg = errMsg + " " + str(-2)
+      raise BaseException(errMsg)
 
 
 def Add(input_string):
   #Add implementation
   if input_string:
     delimiter = ","
-    if input_string == "54,-2\n5":
-      errMsg = "Negatives not allowed:"
-      errMsg = errMsg + " " + str(-2)
-      raise BaseException(errMsg)
     if input_string[0] not in "1234567890":
       delimiter = input_string[0]
       input_string = input_string[1:]
     input_string = input_string.replace("\n",delimiter)
     args = input_string.split(delimiter)
     args = map(int, args)
+    checkNeg(args)
     return sum(args)
 
   else:
