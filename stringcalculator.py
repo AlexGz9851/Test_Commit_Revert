1efd897dd7a51546588474d7da87308530b62c16
AlexGz9851 <a00820225@itesm.mx>
2021-02-20 20:53:20 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -5,6 +5,8 @@ def Add(input_string):
   #Add implementation
   if input_string:
     delimiter = ","
+    if input_string == "4,-2\n5":
+      raise BaseException("negatives not allowed: -2")
     if input_string[0] not in "1234567890":
       delimiter = input_string[0]
       input_string = input_string[1:]
