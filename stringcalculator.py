1a5c787ce32112bd1ce21a6fcc9d77d99484cb65
AlexGz9851 <a00820225@itesm.mx>
2021-02-20 20:14:38 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -4,6 +4,7 @@
 def Add(input_string):
   #Add implementation
   if input_string:
+    input_string = input_string.replace("\n",",")
     args = input_string.split(",")
     return sum(map(int, args))
 
