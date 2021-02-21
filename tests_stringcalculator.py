ae3cb0676518f576d5bd9e771560e9e6570db42b
AlexGz9851 <a00820225@itesm.mx>
2021-02-20 20:00:12 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -7,6 +7,9 @@ class TestStringMethods(unittest.TestCase):
     def test_zero_args(self):
         self.assertEqual(stringcalculator.Add(""), 0)
 
+    def test_one_args(self):
+        self.assertEqual(stringcalculator.Add("3"), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
