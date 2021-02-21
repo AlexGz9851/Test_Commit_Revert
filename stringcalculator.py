d89dbfb37a9bc7a3c0d84e434c27d0f1077286f0
AlexGz9851 <a00820225@itesm.mx>
2021-02-20 20:19:25 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -4,6 +4,8 @@
 def Add(input_string):
   #Add implementation
   if input_string:
+    if input_string[0] not in "1234567890":
+      return 11
     input_string = input_string.replace("\n",",")
     args = input_string.split(",")
     return sum(map(int, args))
